56783ffeeb4d45b08dddaef49b3b35ad15018b32
shorv <kontakt.shorv@gmail.com>
2024-03-23 21:02:21 +0100

    initial commit

---
+++ src/main/java/io/github/aieducationbackend/AiEducationBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.aieducationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiEducationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiEducationBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/aieducationbackend/AiEducationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aieducationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiEducationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
